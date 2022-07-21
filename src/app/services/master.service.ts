abc282e0fee688ca9819a231d4763c1f8be78b23
markoblazevski <marko_blazevski@live.com>
2022-07-21 12:46:58 +0200

    added master

---
+++ src/app/services/master.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {Observable, tap} from "rxjs";
+import {Master} from "../interfaces/Master";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MasterService {
+
+  constructor(private http : HttpClient) { }
+
+  url = 'http://localhost:8080/api/masters'
+
+  getMasters() : Observable<Master[]> {
+    return this.http.get<Master[]>(this.url).pipe(
+      tap(data => console.log(data))
+    )
+  }
+
+}
