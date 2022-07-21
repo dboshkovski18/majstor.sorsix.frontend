a1b3d58b162bc511a66e4033407dbcf19e789b96
markoblazevski <marko_blazevski@live.com>
2022-07-21 12:29:23 +0200

    added master

---
+++ src/app/interfaces/Master.ts
@@ -0,0 +1,10 @@
+export interface Master {
+  name : string,
+  surname : string,
+  phone_number : string,
+  embg : number,
+  gender : string,
+  type : string,
+  city : number,
+  email : string
+}
